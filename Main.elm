55c7b3e9fa12a5b384642b32a53697fa1eb2ff5f
Nathan Kleekamp <nkleekamp@gmail.com>
2017-07-30 15:37:30 -0400

    initial commit

---
+++ Main.elm
@@ -0,0 +1,11 @@
+import Html exposing (Html, div, h1, text)
+import Html.Attributes exposing (..)
+
+main : Html msg
+main =
+  div
+    []
+    [ h1
+      []
+      [ text "Hello World" ]
+    ]
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Minimal Elm Routing
